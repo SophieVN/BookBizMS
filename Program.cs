70525a77a559576dce576b21f445fcb08807e683
Sophie Bai <101148052+SophieVN@users.noreply.github.com>
2022-10-05 23:18:59 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BookBizMS.GUI;
+
+namespace BookBizMS
+{
+   
+
+    static class Program
+    {
+        public static HomePage homePage;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            homePage = new HomePage();
+            Application.Run(homePage);
+        }
+    }
+}
